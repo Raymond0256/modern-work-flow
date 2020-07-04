3190a7de01c5d14ca14e38401e121f1c5f43f599
Raymond Lewis <Raymond0256@gmail.com>
2020-07-04 12:43:53 -0500

    add test_main_fails_on request_error.

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,3 +39,9 @@ def test_main_uses_en_wikipedia_org(runner, mock_request_get):
     runner.invoke(console.main)
     args, _ = mock_request_get.call_args
     assert "en.wikipedia.org" in args[0]
+
+
+def test_main_fails_on_request_error(runner, mock_request_get):
+    mock_request_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
