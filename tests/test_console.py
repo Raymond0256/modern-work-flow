ef74badb80006b1e96aa726ea926fcba3e384fa2
Raymond Lewis <Raymond0256@gmail.com>
2020-07-04 12:39:18 -0500

    Add test_main_prints_title.

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,6 +10,21 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+@pytest.fixture
+def mock_request_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolar sit amet",
+    }
+    return mock
+
+
+def test_main_succeeds(runner, mock_request_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_request_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
